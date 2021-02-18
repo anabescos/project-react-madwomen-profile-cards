fd426e2fc025bf7c942cc53220e1dab0846a637e
anabescos <anabes88@gmail.com>
2021-02-18 15:01:04 +0100

    Add share branch

---
+++ src/components/Share.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../images/MadWomenLogo_2.svg';
+import "../stylesheets/layout/_headerCard.scss";
+
+class Share extends React.Component {
+  render() {
+    return (
+        <header className="header">
+        <a href="./index.html">
+          <img
+            className="header__img"
+            src={logo}
+            title="Go to home page"
+            alt="Mad Women"
+          />
+        </a>
+      </header>
+    );
+  }
+}
+
+export default Share;
+
+
