c46f2f3c6ca39cc314de6cf567647f80bb50a339
PaulaPSoto <paezsotopaula@gmail.com>
2021-02-18 13:38:55 +0100

    Create branch footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+<footer className="footer">
+      <small className="footer__small"> Mad Women profile cards </small>
+      <a
+        className="footer__link"
+        title="Ir a la página de Adalab"
+        type="_blank"
+        href="https://adalab.es/"
+      >
+        <img
+          className="footer__logo"
+          src={adalabLogo}
+          alt="logo Adalab"
+        />
+      </a>
+    </footer>
