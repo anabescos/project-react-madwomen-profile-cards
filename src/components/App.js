25b7767d171767c32db0deda76e820853d6c3d44
anabescos <anabes88@gmail.com>
2021-02-18 15:18:19 +0100

    Add share component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../stylesheets/App.scss';
 
 import adalabLogo from '../images/logo-adalab.png';
 import HeaderCard from './HeaderCard';
+import Share from './Share';
 function App() {
   return (
     <div className="App">
@@ -199,39 +200,7 @@ function App() {
         </fieldset>
 
         {/* <!--Secction share--> */}
-        <fieldset className="share js-container collapsable__close">
-          <legend className="share__title js-title">
-            <div className="share__title--container">
-              <i className="icon fas fa-share-alt"></i>
-              <h2 className="word">Comparte</h2>
-            </div>
-            <i className="collapsable__icon fas fa-chevron-up arrow"></i>
-          </legend>
-
-
-          <div className="border"></div> 
-
-          <div className="share__box collapsable__content collapsable__open">
-            <button
-              className="share__box--button js-create-btn"
-              type="submit"
-              title="Create card"
-            >
-              <i className="iconCard far fa-address-card"></i>
-              Crear tarjeta
-            </button>
-            <p className="js-required"></p>
-            <div className="share__box--subsection js-hidden js-display">
-              <h3 className="titleCard">La tarjeta ha sido creada:</h3>
-              <a className="linkCard js-card-result" href="" target="_blank"
-                >link awesome profile card</a
-              >
-              <a className="twitterLink" href="" target="_blank"
-                ><i className="icon fab fa-twitter"></i>Compartir en twitter</a
-              >
-            </div>
-          </div>
-        </fieldset>
+        <Share />
 
       </form>
 
--- src/components/Share.js
+++ src/components/Share.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import logo from '../images/MadWomenLogo_2.svg';
-import "../stylesheets/layout/_headerCard.scss";
+
+import "../stylesheets/layout/form/_share.scss";
 
 class Share extends React.Component {
   render() {
     return (
-        <header className="header">
-        <a href="./index.html">
-          <img
-            className="header__img"
-            src={logo}
-            title="Go to home page"
-            alt="Mad Women"
-          />
-        </a>
-      </header>
+        <fieldset className="share js-container collapsable__close">
+          <legend className="share__title js-title">
+            <div className="share__title--container">
+              <i className="icon fas fa-share-alt"></i>
+              <h2 className="word">Comparte</h2>
+            </div>
+            <i className="collapsable__icon fas fa-chevron-up arrow"></i>
+          </legend>
+
+
+          {/* <div className="border"></div>  */}
+
+          <div className="share__box collapsable__content collapsable__open">
+            <button
+              className="share__box--button js-create-btn"
+              type="submit"
+              title="Create card">
+              <i className="iconCard far fa-address-card"></i>
+              Crear tarjeta
+            </button>
+            <p className="js-required"></p>
+            <div className="share__box--subsection js-hidden js-display">
+              <h3 className="titleCard">La tarjeta ha sido creada:</h3>
+              <a className="linkCard js-card-result" href="" target="_blank">link awesome profile card</a>
+              <a className="twitterLink" href="" target="_blank"><i className="icon fab fa-twitter"></i>Compartir en twitter</a>
+            </div>
+          </div>
+        </fieldset>
     );
   }
 }
