544452f6ac762ffd1f9f167c15d6ad2bc34f15f7
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-02-20 12:48:21 +0100

    Add Design and Palette components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
-import '../stylesheets/App.scss';
+import "../stylesheets/App.scss";
 
-import adalabLogo from '../images/logo-adalab.png';
-import HeaderCard from './HeaderCard';
-import Fill from './Fill';
-import Design from './Design';
+import adalabLogo from "../images/logo-adalab.png";
+import HeaderCard from "./HeaderCard";
+import Fill from "./Fill";
+import Design from "./Design";
 
-function App () {
+function App() {
   return (
     <div className="App">
       <HeaderCard />
-
       <main className="wrapper">
         <section className="generator">
           <article className="generator__article">
@@ -54,144 +53,10 @@ function App () {
             </div>
           </article>
         </section>
-
         <form className="form js-submit" action="create card" method="GET">
-          {/* <!-- fill section--> */}
           <Design />
-          <fieldset className="fill js-container collapsable__close collapsable__open">
-            {/* <legend className="fill__title js-title">
-            <div className="fill__title--container">
-              <i className="far fa-keyboard icon"></i>
-              <h2 className="word">Rellena</h2>
-            </div>
-            <i className="collapsable__icon fas fa-chevron-up arrow"></i>
-          </legend> */}
-
-            <Fill />
-
-            <div className="fill__box collapsable__content">
-              <label className="fill__box--label name" for="Name">
-                Nombre Completo
-              </label>
-              <input
-                className="fill__box--input js-input-name js-input-text"
-                placeholder="Ej: Sally Jill"
-                type="text"
-                id="name"
-                name="name"
-                required
-              />
-              <label className="fill__box--label" for="job">
-                Puesto
-              </label>
-              <input
-                className="fill__box--input job js-input-job js-input-text"
-                placeholder="Ej: Front-end unicorn"
-                type="text"
-                id="job"
-                name="job"
-                required
-              />
-              <label className="fill__box--label imgLabel" for="photo">
-                Imagen de perfil
-              </label>
-              <div className="addImage">
-                <button
-                  className="js__profile-trigger fill__box--input buttonImg"
-                  type="button"
-                >
-                  Añadir imagen
-                </button>
-                <input
-                  className="js__profile-upload-btn buttonInput"
-                  type="file"
-                  id="photo"
-                  name="photo"
-                  required
-                />
-                <div className="js__profile-preview preview" />
-              </div>
-              <label className="fill__box--label" for="email">
-                Email
-              </label>
-              <input
-                className="fill__box--input js-input-email js-input-text"
-                placeholder="Ej: sally-hill@gmail.com"
-                type="email"
-                id="email"
-                name="email"
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                required
-              />
-              <label className="fill__box--label" for="phone">
-                Teléfono
-              </label>
-              <input
-                className="fill__box--input js-input-phone js-input-text"
-                placeholder="Ej:+34 666666666"
-                type="tel"
-                id="phone"
-                name="phone"
-                pattern="^[0-9]{9}$"
-                required
-              />
-              <label className="fill__box--label" for="linkedin">
-                Linkedin
-              </label>
-              <input
-                className="fill__box--input js-input-linkedin js-input-text"
-                placeholder="Ej: linkedin.com/in/sally.hill"
-                type="text"
-                id="linkedin"
-                name="linkedin"
-              />
-              <label className="fill__box--label" for="github">
-                Github
-              </label>
-              <input
-                className="fill__box--input js-input-github js-input-text"
-                placeholder="Ej: @sally-hill"
-                type="text"
-                id="github"
-                name="github"
-              />
-            </div>
-          </fieldset>
-
-          {/* <!--Secction share-->*/}
-          <fieldset className="share js-container collapsable__close">
-            <legend className="share__title js-title">
-              <div className="share__title--container">
-                <i className="icon fas fa-share-alt" />
-                <h2 className="word">Comparte</h2>
-              </div>
-              <i className="collapsable__icon fas fa-chevron-up arrow" />
-            </legend>
-
-            <div className="border" />
-
-            <div className="share__box collapsable__content collapsable__open">
-              <button
-                className="share__box--button js-create-btn"
-                type="submit"
-                title="Create card"
-              >
-                <i className="iconCard far fa-address-card" />
-                Crear tarjeta
-              </button>
-              <p className="js-required" />
-              <div className="share__box--subsection js-hidden js-display">
-                <h3 className="titleCard">La tarjeta ha sido creada:</h3>
-                <a className="linkCard js-card-result" href="" target="_blank">
-                  link awesome profile card
-                </a>
-                <a className="twitterLink" href="" target="_blank">
-                  <i className="icon fab fa-twitter" />
-                  Compartir en twitter
-                </a>
-              </div>
-            </div>
-          </fieldset>
+          <Fill />
+          {/* <!--Section share-->*/}
         </form>
       </main>
       <footer className="footer">
--- src/components/Design.js
+++ src/components/Design.js
@@ -1,68 +1,75 @@
-import React from 'react';
-import Titles from './Titles';
-import '../stylesheets/layout/form/_design.scss';
+import React from "react";
+import Titles from "./Titles";
+import Palette from "./Palette";
 
+import "../stylesheets/layout/form/_design.scss";
+
+let isChecked = "checked";
 class Design extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.onChangeRadio = this.onChangeRadio.bind(this);
+  }
+
+  onChangeRadio(ev) {
+    const checkedRadio = ev.target.value;
+    if (checkedRadio) {
+      isChecked = "checked";
+    } else {
+      isChecked = "";
+    }
+    console.log(isChecked);
+    this.forceUpdate();
+  }
+
+  render() {
     return (
-      <fieldset className="design js-container collapsable__close">
+      <fieldset className="design js-container ">
+        {/*collapsable__close */}
         <Titles name="Diseña" />
         <div className="design__palette collapsable__content">
           <h3 className="design__palette--colors">Colores</h3>
           <div>
-            <label className="design__palette--input" for="">
+            <Palette>
               <input
-                className="design__palette1 js-palette"
+                className="js-palette design__palette1"
                 type="radio"
                 name="palette"
                 value="1"
-                checked
+                checked={isChecked}
+                onChange={this.onChangeRadio}
               />
-              <ul className="design__palette--list design__palette1">
-                <li className="design__palette1--color1">colorA</li>
-                <li className="design__palette1--color2">colorB</li>
-                <li className="design__palette1--color3">colorC</li>
-              </ul>
-            </label>
-            <label className="design__palette--input" for="">
+            </Palette>
+            <Palette>
               <input
                 className="js-palette"
                 type="radio"
                 name="palette"
                 value="2"
+                checked={isChecked}
+                onChange={this.onChangeRadio}
               />
-              <ul className="design__palette--list">
-                <li className="design__palette2--color1">colorA</li>
-                <li className="design__palette2--color2">colorB</li>
-                <li className="design__palette2--color3">colorC</li>
-              </ul>
-            </label>
-            <label className="design__palette--input" for="">
+            </Palette>
+            <Palette>
               <input
                 className="js-palette"
                 type="radio"
                 name="palette"
                 value="3"
+                checked={isChecked}
+                onChange={this.onChangeRadio}
               />
-              <ul className="design__palette--list">
-                <li className="design__palette3--color1">colorA</li>
-                <li className="design__palette3--color2">colorB</li>
-                <li className="design__palette3--color3">colorC</li>
-              </ul>
-            </label>
-            <label className="design__palette--input" for="">
+            </Palette>
+            <Palette>
               <input
                 className="js-palette"
                 type="radio"
                 name="palette"
                 value="4"
+                checked={isChecked}
+                onChange={this.onChangeRadio}
               />
-              <ul className="design__palette--list">
-                <li className="design__palette4--color1">colorA</li>
-                <li className="design__palette4--color2">colorB</li>
-                <li className="design__palette4--color3">colorC</li>
-              </ul>
-            </label>
+            </Palette>
           </div>
         </div>
       </fieldset>
