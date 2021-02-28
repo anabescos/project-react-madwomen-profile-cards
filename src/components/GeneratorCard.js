89144bd1627398d567767278e12336cc40ab1afb
jechfernandez <jechfernandez@gmail.com>
2021-02-28 13:54:39 +0100

    Fix job input, start working on reset button

--- src/components/GeneratorCard.js
+++ src/components/GeneratorCard.js
@@ -5,17 +5,27 @@ import Form from "./Form";
 import "../stylesheets/layout/_generatorCard.scss";
 
 const GeneratorCard = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
+  const [state, setState] = useState('');
 
   const handleInput = (inputKey, inputValue) => {
     setInput({ [inputKey]: inputValue });
   };
 
+  const handleReset = () => {
+
+    console.log ("Me han clickao");
+    setState({
+      name: '',
+      job: '',
+    })
+  }
+
   return (
     <>
       <section className="generator">
         <article className="generator__article">
-          <Reset />
+          <Reset handleReset={handleReset} state={state}/>
           <Card input={input} />
         </article>
       </section>
--- src/components/Reset.js
+++ src/components/Reset.js
@@ -3,9 +3,10 @@ import React from "react";
 class Reset extends React.Component {
   render() {
     return (
-      <div className="generator__article--button1 js-reset">
-        <i className="far fa-trash-alt"></i>Reset
-      </div>
+      <button className="generator__article--button1 js-reset" onClick={this.props.handleReset}>
+        <i className="far fa-trash-alt"></i>
+        <span>Reset</span>
+      </button>
     );
   }
 }
