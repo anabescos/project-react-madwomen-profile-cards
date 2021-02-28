dc22e64d9a9f5d7826f7eab0a1b0763240c6e184
jechfernandez <jechfernandez@gmail.com>
2021-02-28 19:28:44 +0100

    Fix reset button

--- src/components/GeneratorCard.js
+++ src/components/GeneratorCard.js
@@ -16,17 +16,32 @@ class GeneratorCard extends React.Component {
       github: "",
     };
     this.handleInput = this.handleInput.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleInput(inputKey, inputValue) {
     this.setState({ [inputKey]: inputValue });
   }
+
+  handleReset(){
+    this.setState ({
+      name: '',
+      job: '',
+      email: '',
+      phone: '',
+      linkedin: '',
+      github: '',
+    });
+  }
+
+  
+
   render() {
     return (
       <>
         <section className="generator">
           <article className="generator__article">
-            <Reset />
+            <Reset handleReset={this.handleReset}/>
             <Card data={this.state} handleInput={this.handleInput} />
           </article>
         </section>
