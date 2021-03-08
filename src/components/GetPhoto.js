08d13f57f69b3c9f73eb9de27e0525d93af3b053
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-03-08 21:11:27 +0100

    Add image functionality

--- src/components/GetPhoto.js
+++ src/components/GetPhoto.js
@@ -1,74 +1,74 @@
-import React from 'react';
+import React from "react";
 // import PropTypes from 'prop-types';
-import defaultPhoto from '../images/peggy-olson.jpg';
-import '../stylesheets/layout/form/GetPhoto.scss';
+import defaultPhoto from "../images/peggy-olson.jpg";
+import "../stylesheets/layout/form/GetPhoto.scss";
 
 class GetPhoto extends React.Component {
-  constructor (props) {
-    super (props);
-    this.fr = new FileReader ();
+  constructor(props) {
+    super(props);
+    this.fr = new FileReader();
 
-    this.myFileField = React.createRef ();
+    this.myFileField = React.createRef();
 
-    this.uploadImage = this.uploadImage.bind (this);
-    this.getImage = this.getImage.bind (this);
+    this.uploadImage = this.uploadImage.bind(this);
+    this.getImage = this.getImage.bind(this);
   }
 
-  uploadImage (ev) {
-    console.log ('La usuaria ha abierto la ventana para elegir ficheros');
+  uploadImage(ev) {
+    console.log("La usuaria ha abierto la ventana para elegir ficheros");
 
-    console.log ('La usuaria ha elegido los ficheros', ev.currentTarget.files);
+    console.log("La usuaria ha elegido los ficheros", ev.currentTarget.files);
 
-    console.log (
-      'El primero de los ficheros elegidos es',
+    console.log(
+      "El primero de los ficheros elegidos es",
       ev.currentTarget.files[0]
     );
 
     if (ev.currentTarget.files.length > 0) {
       const myFile = ev.currentTarget.files[0];
 
-      this.fr.addEventListener ('load', this.getImage);
+      this.fr.addEventListener("load", this.getImage);
 
-      this.fr.readAsDataURL (myFile);
+      this.fr.readAsDataURL(myFile);
     }
   }
 
-  getImage () {
-    console.log ('Información útil sobre el fichero cargado', this.fr);
+  getImage() {
+    console.log("Información útil sobre el fichero cargado", this.fr);
 
     const image = this.fr.result;
 
     this.props.updatePhoto(image);
   }
 
-  render () {
-    const photo = this.props.photo === '' ? defaultPhoto : this.props.photo;
+  render() {
+    const photo = this.props.photo === "" ? defaultPhoto : this.props.photo;
     return (
-      <div className="addImage">
-
-        <label className="fill__box--label imgLabel" htmlFor="photo">
-          Imagen de perfil
-        </label>
-        <button
-          className="js__profile-trigger fill__box--input buttonImg"
-          type="button"
-        >
-          Añadir imagen
-        </button>
-        <input
-          className="js__profile-upload-btn buttonInput"
-          type="file"
-          ref={this.myFileField}
-          onChange={this.uploadImage}
-          id="photo"
-          name="photo"
-          required
-        />
-        <div
-          className="js__profile-preview preview"
-          style={{ backgroundImage: `url(${photo})` }}
-        />
-      </div>
+      <>
+        <div className="fill__box--label imgLabel">Imagen de perfil</div>
+        <div className="addImage">
+          <label
+            type="button"
+            htmlFor="photo"
+            className="js__profile-trigger fill__box--input buttonImg"
+          >
+            Añadir imagen
+            <input
+              className="js__profile-upload-btn buttonInput"
+              type="file"
+              ref={this.myFileField}
+              onChange={this.uploadImage}
+              id="photo"
+              name="photo"
+              required
+            />
+          </label>
+          <div
+            className="js__profile-preview preview"
+            style={{ backgroundImage: `url(${photo})` }}
+          />
+        </div>
+      </>
     );
   }
 }
