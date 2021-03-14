c28059c48bc9d3af303ff67dde5e3a3b03f23262
PaulaPSoto <paezsotopaula@gmail.com>
2021-03-14 10:32:30 +0100

    Add server

--- server/src/index.js
+++ server/src/index.js
@@ -1,47 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 
-// hacer aquí la petición de los datos del array de usuario obtenidos.
-
-// SERVER
-
-// config server
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-// init express aplication
 const serverPort = 3000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-const staticServerpath = "./public";
-server.use(express.static(staticServerpath));
 
-// API
-server.get("/api/products", (req, res) => {
-  // Cojo los productos y los devuelvo
-  // res.json(productsData);
+server.get("/card/:id:/", (req, res) => {
+  const response = {
+    users: [{ name: "Sofía" }, { name: "María" }],
+  };
+  res.json(response);
 });
 
-server.post("/api/user/login", (req, res) => {
-  // Cojo los datos que recibo desde el navegador que están en req.body.email y req.body.password
-  // Con estos datos busco en el array de usuarios si el usuario existe
-  // const userFound = usersData.find((user) => {
-  //   return user.email === req.body.email && user.password === req.body.password;
-  // });
-  // if (userFound !== undefined) {
-  //   // Si el usuario existe devuelvo el id del usuario
-  //   res.json({
-  //     error: false,
-  //     userId: userFound.id,
-  //   });
-  // } else {
-  //   // Si el usuario no existe devuelvo un error
-  //   res.status(404).json({
-  //     error: "user-not-found",
-  //     message: "User not found",
-  //   });
-  // }
-});
+ server.post('/card/', (req, res) => {
+//     // Cojo los datos que recibo desde el navegador que están en req.body.email y req.body.password
+//     // Con estos datos busco en el array de usuarios si el usuario existe
+//     const userFound = usersData.find(user => {
+//       return user.email === req.body.email && user.password === req.body.password;
+//     });
+
+//     if (userFound !== undefined) {
+//       // Si el usuario existe devuelvo el id del usuario
+//       res.json({
+//         error: false,
+//         userId: userFound.id
+//       });
+//     } else {
+//       // Si el usuario no existe devuelvo un error
+//       res.status(404).json({
+//         error: 'user-not-found',
+//         message: 'User not found'
+//       });
+//     }
+//   });
